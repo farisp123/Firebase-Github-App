dd43d4aad80da184cb19d2c045db7c7ff409dfcb
Muhammed Faris P <102180559+farisp123@users.noreply.github.com>
2023-06-06 01:12:09 +0530

    Update firebaseConfig.js

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -1,9 +1,1 @@
- export const firebaseConfig = {
-    apiKey: "AIzaSyCl8tgwfUFmvNypL8fVgG9zJ6SCu4Spls8",
-    authDomain: "gitapp-5df79.firebaseapp.com",
-    projectId: "gitapp-5df79",
-    storageBucket: "gitapp-5df79.appspot.com",
-    messagingSenderId: "611178379733",
-    appId: "1:611178379733:web:700fa5b9811027370ea55c",
-    measurementId: "G-P6TSY0ZJ9S"
-  };
+// Provide the firebase config file  to access the firebase
